090fc7e9fb79159657376be896ab874c549c0817
Ram <rr7patel@gmail.com>
2018-07-22 01:26:28 -0400

    added posterior and prior distributions

--- src/models/rvae.py
+++ src/models/rvae.py
@@ -10,8 +10,8 @@ FLAGS = tf.app.flags.FLAGS
 class RVAE(object):
     """ Builds the model graph for different modes(train, eval, predict) """
     def __init__(self, hps, vocab_size):
-        self.hps = hps
-        self.vsize = vocab_size #TODO: Change this/find a better way to pass in vocab_size
+        self._hps = hps
+        self._vsize = vocab_size #TODO: Change this/find a better way to pass in vocab_size
 
     def _add_source_encoder(self, input, seq_len, hidden_dim, initializer):
         """ Adds a single-layer bidirectional LSTM encoder to parse the original sentence(source_seq)
@@ -49,9 +49,18 @@ class RVAE(object):
             (_, (fw_st, bw_st)) = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, input, initial_state_fw=fw_st, initial_state_bw=bw_st, dtype=tf.float32, sequence_length=seq_len, swap_memory=True)
         return fw_st, bw_st
 
-    def vae(self, input):
-        """ Builds and creates the variational autoencoder ops """
-        return NotImplementedError
+    def _make_posterior(self, enc_output, latent_dim, initializaer):
+        """ Builds ops to calculate the posterior and sample from it
+        Args:
+            enc_output: `Tensor` of batch size (batch_size, hidden_dim*2) from encoder
+        Returns:
+            A tf.distributions.MultivariateNormalDiag representing the posterior for each seq
+        """
+        # get mu and sigma
+        mu = tf.layers.dense(enc_output, latent_dim, kernel_initializer=initializaer) # mean with shape (batch_size, latent_dim)
+        sigma = tf.layers.dense(enc_output, latent_dim, tf.nn.softplus, kernel_initializer=initializaer) # logvar with shape (batch_size, latent_dim)
+
+        return tf.contrib.distributions.MultivariateNormalDiag(loc=mu, scale_diag=sigma)
 
     def decoder(self, input, mode):
         """ Creates a decoder to produce outputs """
@@ -87,7 +96,7 @@ class RVAE(object):
 
         # embed all necessary input tensors
         with tf.variable_scope('embedding'):
-            embedding = tf.get_variable('embedding', [self.vsize, self.hps.emb_dim], dtype=tf.float32, initializer=embedding_init) # initialize with pretrained word vecs
+            embedding = tf.get_variable('embedding', [self._vsize, self._hps.emb_dim], dtype=tf.float32, initializer=embedding_init) # initialize with pretrained word vecs
             emb_src_inputs = tf.nn.embedding_lookup(embedding, features['source_seq']) # (batch_size, max_seq_len, emb_dim)
             if mode != tf.estimator.ModeKeys.PREDICT:
                 emb_dec_inputs = [tf.nn.embedding_lookup(embedding, x) for x in tf.unstack(labels['target_seq'])]
@@ -96,21 +105,26 @@ class RVAE(object):
                     emb_tgt_inputs = tf.nn.embedding_lookup(embedding, labels['target_seq']) # (batch_size, max_seq_len, emb_dim) for target encoding
 
         # pass the embedded tensors to the source encoder
-        src_fw_st, src_bw_st = self._add_source_encoder(emb_src_inputs, features['source_len'], self.hps.hidden_dim, rand_unif_init)
+        src_fw_st, src_bw_st = self._add_source_encoder(emb_src_inputs, features['source_len'], self._hps.hidden_dim, rand_unif_init)
 
         # pass embedded tensors to the target encoder
         if mode == tf.estimator.ModeKeys.TRAIN:
-            tgt_fw_st, tgt_bw_st = self._add_target_encoder(emb_tgt_inputs, src_fw_st, src_bw_st, labels['target_len'], self.hps.hidden_dim, rand_unif_init)
+            tgt_fw_st, tgt_bw_st = self._add_target_encoder(emb_tgt_inputs, src_fw_st, src_bw_st, labels['target_len'], self._hps.hidden_dim, rand_unif_init)
             enc_output = tf.concat([tgt_fw_st, tgt_bw_st], 1) # shape (batch_size, hidden_dim*2)
         else:
             enc_output = tf.concat([src_fw_st, src_bw_st], 1) # shape (batch_size, hidden_dim*2)
 
-        # pass through the linear layer and calculate mean and std 
-        # TODO: Put into function, calculate prior, and return the MultivariateNormalDistribution
-        mu = tf.layers.dense(enc_output, self.hps.latent_dim, kernel_initializer=rand_norm_init) # mean with shape (batch_size, latent_dim)
-        sigma = tf.layers.dense(enc_output, self.hps.latent_dim, tf.nn.softplus, kernel_initializer=rand_norm_init) # logvar with shape (batch_size, latent_dim)
-        q_z = tf.contrib.distributions.MultivariateNormalDiag(mu, sigma)
+        # add the posterior distribution and sample from it
+        q_z = self._make_posterior(enc_output, self._hps.latent_dim, rand_norm_init)
         z = q_z.sample()
+
+        # add the prior distribution for loss
+        p_z = tf.contrib.distributions.MultivariateNormalDiag(loc=[0.]*self._hps.latent_dim,scale_diag=[1.]*self._hps.latent_dim)
+
+        # TODO: Add decoder
+
+        # TODO: Computes losses with KL annealing or something
+
 
 
 
