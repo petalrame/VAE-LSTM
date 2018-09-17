a78057c3b9e80642bb48bdd11861af75605c1d24
Ram <rr7patel@gmail.com>
2018-09-17 14:12:11 -0400

    Added config tweaks

---
+++ src/utils/load_config.py
@@ -0,0 +1,17 @@
+import ruamel_yaml import YAML
+import tensorflow.contrib.training import HParams
+
+class YParams(HParams):
+    def __init__(self, yaml_fn, config_name):
+        super().__init__()
+        with open(yaml_fn) as fp:
+            for k, v in YAML().load(fp)[config_name].items():
+                self.add_hparam(k, v)
+
+class ModelParams(YParams):
+    pass
+
+class AppConfig(YParams):
+    def __init__(self, yaml_fn, config_name):
+        super().__init__(yaml_fn, config_name)
+        self.model_parameter = ModelParams(self.parameter_file,   self.parameter_profile)
